80caab8a564d84447ca4f2d58599e2b080530103
marieparker <lmparker@byui.edu>
2020-01-25 21:47:05 -0700

    update

--- lesson3/js/date.js
+++ lesson3/js/date.js
@@ -4,3 +4,10 @@ try {
 catch (e) {
     alert ["Your browser does not support this function"]
 }
+
+try {
+    document.getElementById('lastUpdated').innerHTML = new Date().toLocaleDateString("en-US") + " " + new Date().toLocaleTimeString("en-US");
+}
+catch (e) {
+    alert ["Your browser does not support this function"]
+}
