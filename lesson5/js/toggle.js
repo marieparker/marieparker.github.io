74b0b429a300abc539e22e2582dc7f6e87b29941
marieparker <lmparker@byui.edu>
2020-02-07 18:09:27 -0700

    update

--- lesson5/js/toggle.js
+++ lesson5/js/toggle.js
@@ -1,11 +1,12 @@
 var day = new Date();
 var d = day.getDay();    
-
-if (d == 5) {
-    document.getElementsByClassName("notyet")[0].classList.toggle("pancakes");
+d=2;
+if (d != 5) {
+    document.getElementById("pancakes").style.display = 'none';
 }
 
 console.log(d);
+
 
 function toggleMenu() {
     document.getElementById("navMenu").classList.toggle("hide");
