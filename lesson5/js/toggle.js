35f780369103ee24fdd0a166d0908573af48b5d0
marieparker <lmparker@byui.edu>
2020-02-07 18:10:11 -0700

    update

--- lesson5/js/toggle.js
+++ lesson5/js/toggle.js
@@ -5,9 +5,6 @@ if (d != 5) {
     document.getElementById("pancakes").style.display = 'none';
 }
 
-console.log(d);
-
-
 function toggleMenu() {
     document.getElementById("navMenu").classList.toggle("hide");
 }
