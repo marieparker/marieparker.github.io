c2815af2a69abc42fe45ef9670baf427193b1ede
marieparker <lmparker@byui.edu>
2020-02-20 11:28:45 -0700

    update

---
+++ lesson7/js/date.js
@@ -0,0 +1,11 @@
+
+const event = new Date();
+
+const options = {weekday:'long', day:'numeric', month:'long', year:'numeric'};
+
+try {
+    document.getElementById('lastUpdated').innerHTML = event.toLocaleDateString('en-GB', options);
+}
+catch (e) {
+    alert ["Your browser does not support this funtion"]
+}
---
+++ lesson7/lazyload/lazyload.js
@@ -0,0 +1,56 @@
+const images = document.querySelectorAll("[data-src]");
+
+function preloadImage(img) {
+    const src = img.getAttribute("data-src");
+    if(!src) {
+        return;
+    }
+
+	img.src = src;
+	img.onload = () => {
+		img.removeAttribute('data-src');
+}
+}
+const imgOptions = {};
+
+const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+    entries.forEach(entry => {
+        if(!entry.isIntersecting) {
+            return;
+        }
+        else {
+            preloadImage(entry.target);
+            imgObserver.unobserve(entry.target);
+        }
+    })
+}, imgOptions);
+
+images.forEach(image => {
+    imgObserver.observe(image);
+})
+
+
+/*loadImages = function(image) {
+	image.setAttribute('src', image.getAttribute('data-src'));
+	image.onload = function() {
+		image.removeAttribute('data-src');
+	};
+};
+if('IntersectionObserver' in window) {
+	var observer = new IntersectionObserver(function(items, observer) {
+		items.forEach(function(item) {
+			if(item.isIntersecting) {
+				loadImages(item.target);
+				observer.unobserve(item.target);
+			}
+		});
+	});
+	imagesToLoad.forEach(function(img) {
+		observer.observe(img);
+	});
+}
+else {
+	imagesToLoad.forEach(function(img) {
+		loadImages(img);
+	});
+}*/
