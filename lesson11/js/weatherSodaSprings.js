3ac35fd2c848cd9bf7a6d2bda9163d08ca5bfb84
marieparker <lmparker@byui.edu>
2020-03-17 14:50:27 -0700

    update

--- lesson11/js/weatherSodaSprings.js
+++ lesson11/js/weatherSodaSprings.js
@@ -4,18 +4,11 @@ fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
-
-    /*document.getElementById('current-temp').textContent = jsObject.main.temp;*/
+    
     document.getElementById('currentTemp').textContent = Math.round(jsObject.main.temp);
     document.getElementById('Temp').textContent = Math.round(jsObject.main.temp_max);
     document.getElementById('humidity').textContent = Math.round(jsObject.main.humidity);
     document.getElementById('windSpeed').textContent = Math.round(jsObject.wind.speed);
-
-    /*const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
-    const desc = jsObject.weather[0].description;  // note how we reference the weather array
-    document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
-    document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
-    document.getElementById('icon').setAttribute('alt', desc);*/
   
 /*Calculate Wind Chill*/
 const temp = document.getElementById('currentTemp').textContent;
