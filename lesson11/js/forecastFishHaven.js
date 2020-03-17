bcbeebb20707e182c72e4f59b7a04ad777b70052
marieparker <lmparker@byui.edu>
2020-03-17 14:49:33 -0700

    update

---
+++ lesson11/js/forecastFishHaven.js
@@ -0,0 +1,33 @@
+const forecastApiURL = "https://api.openweathermap.org/data/2.5/forecast?id=5585000&cnt=40&units=imperial&appid=32b36f37051b2578eec25a9c85d88e8a"
+fetch(forecastApiURL)
+  .then((response) => response.json())
+  .then((forecastData) => {
+    console.log(forecastData); 
+
+    var g = 1;
+
+    var today = new Date();
+    var day = today.getDay();
+    var daylist = ["Sun", "Mon", "Tues", "Wed", "Thur", "Fri", "Sat"];
+    console.log(day);
+    
+for (i=1; i<6; i++) {
+    var forecastDay = day + i;
+    if (forecastDay >= 7) {
+        forecastDay = forecastDay - 7;
+    }
+    document.getElementById('day' + i).textContent = daylist[forecastDay];
+}
+for (i=0; i<40; i++) {
+    if (forecastData.list[i].dt_txt.includes("18:00:00")) {
+        var imagesrc = 'https://openweathermap.org/img/w/' + forecastData.list[i].weather[0].icon + '.png';
+        var desc = forecastData.list[i].weather[0].description;
+        
+        document.getElementById('icon' + g).setAttribute('src', imagesrc);
+        document.getElementById('icon' + g).setAttribute('alt', desc);
+
+        document.getElementById('temp' + g).textContent = Math.round(forecastData.list[i].main.temp); 
+        g=g+1;}
+}
+
+});
---
+++ lesson11/js/weatherFishHaven.js
@@ -0,0 +1,33 @@
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5585000&units=imperial&appid=32b36f37051b2578eec25a9c85d88e8a"
+
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+    
+    document.getElementById('currentTemp').textContent = Math.round(jsObject.main.temp);
+    document.getElementById('Temp').textContent = Math.round(jsObject.main.temp_max);
+    document.getElementById('humidity').textContent = Math.round(jsObject.main.humidity);
+    document.getElementById('windSpeed').textContent = Math.round(jsObject.wind.speed);
+  
+/*Calculate Wind Chill*/
+const temp = document.getElementById('currentTemp').textContent;
+const wind = document.getElementById('windSpeed').textContent;
+
+var t = parseFloat(temp);
+var s = parseFloat(wind);
+
+var windChill;
+
+if (t>50 || s<3) {
+    windChill = "N/A"
+    document.getElementById('windChill').innerHTML = windChill;
+}
+else {
+    windChill = 35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s, 0.16);
+    windChill = Math.round(windChill*10)/10;
+    document.getElementById('windChill').innerHTML = windChill + "&deg;";
+}
+
+    
+});
