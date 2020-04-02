fede56ce320c960ad8821bb39fb102393043de4e
marieparker <lmparker@byui.edu>
2020-04-01 19:54:09 -0700

    update

--- final/js/guides.js
+++ final/js/guides.js
@@ -8,19 +8,22 @@ fetch(requestURL)
     console.table(jsonObject); // temporary checking for valid response and data parsing
     const guides = jsonObject['guides']; 
 
+    let row = 1;
+
     for (let i = 0; i < guides.length; i++) {
         let card = document.createElement('div');
         card.setAttribute('class', 'guide');
+        card.setAttribute('id', 'row' + row)
 
         let info = document.createElement('section');
         card.appendChild(info);
         
         let name = document.createElement('h2');
-        name.textContent = guides[0].name;
+        name.textContent = guides[i].name;
         card.appendChild(name);
 
         let cert = document.createElement('p');
-        cert.textContent = guides[0].certLevel + guides[0].experience;
+        cert.textContent = guides[i].certLevel + '\n' +  guides[i].experience;
         card.appendChild(cert);
         console.log(cert);
 
@@ -28,7 +31,13 @@ fetch(requestURL)
         experience.textContent = guides[0].experience;
         card.appendChild(experience);*/
 
+        let photo = document.createElement('img');
+        photo.setAttribute('src', 'images/' + guides[i].picture);
+        photo.setAttributr('alt', 'Picture of ' + guides[i].name);
+
         document.getElementById('guides').appendChild(card);
+
+        row = row + 1;
 
     }
 
