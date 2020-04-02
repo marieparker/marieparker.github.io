3e7c6595cb7cc044eb616865f6fbd154cd599b77
marieparker <lmparker@byui.edu>
2020-04-01 19:46:35 -0700

    update

--- final/js/guides.js
+++ final/js/guides.js
@@ -8,15 +8,27 @@ fetch(requestURL)
     console.table(jsonObject); // temporary checking for valid response and data parsing
     const guides = jsonObject['guides']; 
 
-    for (let i = 1; i < guides.length; i++) {
+    for (let i = 0; i < guides.length; i++) {
         let card = document.createElement('div');
         card.setAttribute('class', 'guide');
 
         let info = document.createElement('section');
         card.appendChild(info);
         
-        let name = document.createAttributr('h2');
+        let name = document.createElement('h2');
         name.textContent = guides[0].name;
+        card.appendChild(name);
+
+        let cert = document.createElement('p');
+        cert.textContent = guides[0].certLevel + guides[0].experience;
+        card.appendChild(cert);
+        console.log(cert);
+
+        /*let experience = document.createElement('p');
+        experience.textContent = guides[0].experience;
+        card.appendChild(experience);*/
+
+        document.getElementById('guides').appendChild(card);
 
     }
 
